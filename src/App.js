5708bf166c11d30d152831d2d065cfefc235ac2f
Ep1cFIN <roope.ripatti@aalto.fi>
2021-10-11 09:09:53 +0300

    added the firebase auth and all that comes with it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-const { SikaKuva } = require('./assets/sikaKuva.png')
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+//import { getFirestore } from 'firebase/firestore'
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+//import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBs-vaqBiC5zL5j4Q6RKECz4xlunLmi8hU",
+  authDomain: "sikapeli-2021.firebaseapp.com",
+  projectId: "sikapeli-2021",
+  storageBucket: "sikapeli-2021.appspot.com",
+  messagingSenderId: "443237001906",
+  appId: "1:443237001906:web:001f50d6c57ee645c737ab"
+})
+
+const auth = getAuth(firebaseApp);
+//const firestore = getFirestore(firebaseApp);
+
 
 function App() {
-  console.log(`Sikakuva = ${SikaKuva}`)
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <p>Sikapeli 2021!!!</p>
-      <SikaKuva/>
+      <header>
+        <h1>Sikagame 2021</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <h1>Olet kirjautunut :)</h1> : <SignIn />}
+      </section>
+
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,4 @@
+//returns a button consisting of onclick function and text as parameters
+const CustomButton = (f, text) => <button onClick={f}>text</button>
+
+export default CustomButton
