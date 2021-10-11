1e6b10bd75b4fe550d0ebcee57f5a04c13d079ac
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-10-11 21:04:44 +0300

    Implement (untested) game logic and Gamestate class

--- src/game-logic/gamestate-variables.js
+++ src/game-logic/gamestate-variables.js
@@ -1,45 +1,15 @@
 const GamestateVariables = {
     PEKONI: 'pekoni',
-    RESETIT: 'resetit',
-    GENERATORS: {
-        SIKALA: 'sikala',
-        REVOLVERI: 'revolveri',
-        COWBOYHATTU: 'cowboyhattu',
-        BOOTSIT: 'bootsit',
-        SALUUNA: 'saluuna',
-        RAUTATIE: 'rautatie',
-        RAUTATIEASEMA: 'rautatieasema',
-        KULTAKAIVOS: 'kultakaivos',    
-    }
+    RESET: 'reset',
+    SIKALA: 'sikala',
+    REVOLVERI: 'revolveri',
+    COWBOYHATTU: 'cowboyhattu',
+    BOOTSIT: 'bootsit',
+    SALUUNA: 'saluuna',
+    RAUTATIE: 'rautatie',
+    RAUTATIEASEMA: 'rautatieasema',
+    KULTAKAIVOS: 'kultakaivos',
 }
 Object.freeze(GamestateVariables)
 
-const ProductionRates = {
-    [GamestateVariables.GENERATORS.SIKALA]: 1,
-    [GamestateVariables.GENERATORS.REVOLVERI]: 2,
-    [GamestateVariables.GENERATORS.COWBOYHATTU]: 3,
-    [GamestateVariables.GENERATORS.BOOTSIT]: 4,
-    [GamestateVariables.GENERATORS.SALUUNA]: 5,
-    [GamestateVariables.GENERATORS.RAUTATIE]: 6,
-    [GamestateVariables.GENERATORS.RAUTATIEASEMA]: 7,
-    [GamestateVariables.GENERATORS.KULTAKAIVOS]: 8,
-}
-Object.freeze(ProductionRates)
-
-const startingGameState = {
-        [GamestateVariables.PEKONI]: 0,
-        [GamestateVariables.RESETIT]: 0,
-        [GamestateVariables.GENERATORS]: {
-            [GamestateVariables.GENERATORS.SIKALA]: 0,
-            [GamestateVariables.GENERATORS.REVOLVERI]: 0,
-            [GamestateVariables.GENERATORS.COWBOYHATTU]: 0,
-            [GamestateVariables.GENERATORS.BOOTSIT]: 0,
-            [GamestateVariables.GENERATORS.SALUUNA]: 0,
-            [GamestateVariables.GENERATORS.RAUTATIE]: 0,
-            [GamestateVariables.GENERATORS.RAUTATIEASEMA]: 0,
-            [GamestateVariables.GENERATORS.KULTAKAIVOS]: 0,    
-        }
-}
-Object.freeze(startingGameState)
-
-export { GamestateVariables, ProductionRates, startingGameState }
+export { GamestateVariables, }
--- src/game-logic/gamestate.js
+++ src/game-logic/gamestate.js
@@ -1,21 +1,73 @@
-import { GamestateVariables, ProductionRates} from './gamestate-variables.js'
+import { GamestateVariables, generatorSpecs } from './gamestate-variables.js'
+import { calculatePrice } from './purchase-logic.js'
 
-function calculateGeneratedBacon(gamestate, frameTime) {
-    const frameMultiplier = frameTime / 1000
-    return Object
-        .values(GamestateVariables.GENERATORS)
-        .reduce((sum, valueName) =>
-            sum + gamestate[valueName] * ProductionRates[valueName] * frameMultiplier,
-            0
-        )
+class Gamestate {
+    /**
+     * Copy initializer or set everything to 0
+     */
+    constructor(initializer = null) {
+        Object.values(GamestateVariables).forEach(variable => {
+            this[variable] = initializer?.[variable] ?? 0
+        })
+    }
+
+    copyAndRaisePropertyByOne(gamestateVariable) {
+        return new Gamestate({...this, [gamestateVariable]: this[gamestateVariable] + 1})
+    }
+
+    canBuy(gamestateVariable) {
+        return this.bacon >= calculatePrice(gamestateVariable, this[gamestateVariable])
+    }
+
+    buy(gamestateVariable) {
+        // Can't afford the thing
+        if (!this.canBuy(gamestateVariable)) {
+            throw new Error(`Not enough currency for ${gamestateVariable}`)
+        }
+
+        let newState = this.copyAndRaisePropertyByOne(gamestateVariable)
+        newState[GamestateVariables.PEKONI] -= calculatePrice(gamestateVariable, newState[gamestateVariable])
+        
+        return newState
+    }
+
+    add(gamestateVariable) {
+        switch(gamestateVariable) {
+            // Clicked the pig
+            case GamestateVariables.PEKONI:
+                return this.copyAndRaisePropertyByOne(GamestateVariables.PEKONI)
+            
+            // Bought reset
+            case GamestateVariables.RESET:
+                // TODO Implement resets
+                throw new Error("Resetit ei toimi vielä! Jos tää lipsui pelin tuotantoversioon, oon pahoillani 😀 Ota yhteyttä sikatiimiin ASAP, pliis!")
+            
+            // Bought anything else
+            default:
+                // This function was called with something not in the GamestateVariables enum for whatever reason
+                if (!(gamestateVariable in Object.values(GamestateVariables))) {
+                    throw new Error(`Invalid gamestate variable: ${gamestateVariable}`)
+                }
+
+                return this.buy(gamestateVariable)
+        }
+    }
+
+    calculateBaconPerSecond() {
+        return Object
+            .keys(generatorSpecs)
+            .reduce((sum, valueName) =>
+                sum + this[valueName] * generatorSpecs[valueName].rate,
+                0
+            )
+    }
+    
+    stepOneFrame(frameTimeinMilliseconds) {
+        var generatedBacon = this.calculateBaconPerSecond() * (frameTimeinMilliseconds / 1000)
+        var newState = new Gamestate(this)
+        newState[GamestateVariables.PEKONI] += generatedBacon
+        return newState
+    }
 }
 
-/*
-Steps adds bacon generated during one frame to the gamestate
-*/
-function stepOneFrame(gamestate, frameTime) {
-    gamestate[GamestateVariables.PEKONI] += calculateGeneratedBacon(gamestate, frameTime)
-    return gamestate
-}
-
-export { generateStartingGamestate, stepOneFrame }
+export { Gamestate, }
