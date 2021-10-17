b6f9877e4cb9947a89cb38122732a0a9f2a9acfc
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-10-17 19:18:42 +0300

    Added background, pig, and counter

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,6 +2,7 @@ import { useState, } from "react";
 
 import { Gamestate, } from '../game-logic/gamestate'
 import { GamestateVariables, } from "../game-logic/gamestate-variables";
+import { SikaKuva, } from './sikaKuva.jsx'
 
 const Game = () => {
     const [gamestate, setGamestate] = useState(new Gamestate())
@@ -10,6 +11,8 @@ const Game = () => {
 
     return (
         <>
+            <p id='bacon-counter'>{gamestate[GamestateVariables.PEKONI]}</p>
+            <SikaKuva handleClick={() => setGamestate(gamestate.add(GamestateVariables.PEKONI))}/>
             {Object.values(GamestateVariables).map((variable) =>
                 <button key={variable} onClick={() => setGamestate(gamestate.add(variable))}>{variable}</button>)}
             <p>{JSON.stringify(gamestate)}</p>
