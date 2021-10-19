2917fb57be8d08b66229cf483fe26311122e9e2c
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-10-19 22:22:37 +0300

    Revert "hide variable names"
    This reverts commit f6a7ec1481b7e798adcb270d0e89b5a29a059e6a.

--- src/game-logic/gamestate-variables.js
+++ src/game-logic/gamestate-variables.js
@@ -1,14 +1,14 @@
 const GamestateVariables = {
-    PEKONI: 'salainen1',
-    RESET: 'salainen2',
-    SIKALA: 'salainen3',
-    REVOLVERI: 'salainen4',
-    COWBOYHATTU: 'salainen5',
-    BOOTSIT: 'salainen6',
-    SALUUNA: 'salainen7',
-    RAUTATIE: 'salainen8',
-    RAUTATIEASEMA: 'salainen9',
-    KULTAKAIVOS: 'salainen10',
+    PEKONI: 'pekoni',
+    RESET: 'reset',
+    SIKALA: 'sikala',
+    REVOLVERI: 'revolveri',
+    COWBOYHATTU: 'cowboyhattu',
+    BOOTSIT: 'bootsit',
+    SALUUNA: 'saluuna',
+    RAUTATIE: 'rautatie',
+    RAUTATIEASEMA: 'rautatieasema',
+    KULTAKAIVOS: 'kultakaivos',
 }
 Object.freeze(GamestateVariables)
 
