2902c8d264d1b9dddb7e5084e571843d17e272cf
Zepritz <92265350+Zepritz@users.noreply.github.com>
2021-10-30 20:49:53 +0300

    Update calculate price function

--- src/game-logic/purchase-logic.js
+++ src/game-logic/purchase-logic.js
@@ -3,19 +3,21 @@ import { generatorSpecs } from "./generators";
 
 const PRICE_POWER = 2
 const RESET_PRICE = Math.pow(10, 15)
-const PRICE_BASE_MULTIPLIER = 10
+const PRICE_BASE_MULTIPLIER = 2
+const BASE_NUMBER = 1.10
 
 /**
  * Calculates the price of the variableCount:th purhcaseable of type gamestateVariable.
  * Indexing of variableCount starts from 1 because it makes sense that 0 items costs 0.
  */
+
 function calculatePrice(gamestateVariable, variableCount) {
-    switch(gamestateVariable) {
-        case GamestateVariables.RESET:
+    switch(true) {
+        case (gamestateVariable === GamestateVariables.RESET):
             return RESET_PRICE
 
         case (gamestateVariable in generatorSpecs):
-            return generatorSpecs[gamestateVariable].basePrice * PRICE_BASE_MULTIPLIER * Math.pow(variableCount, PRICE_POWER)
+            return generatorSpecs[gamestateVariable].basePrice * PRICE_BASE_MULTIPLIER * Math.pow(BASE_COUNT, variableCount)
 
         default:
             throw new Error(`Invalid gamestate variable: ${gamestateVariable}`)
