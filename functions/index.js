40991f0494af753bc552b7af6acd9755dc6d68a7
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-11-06 00:36:10 +0200

    Add function config, remove unnecessary await

--- functions/index.js
+++ functions/index.js
@@ -46,7 +46,7 @@ function countTime(modifications) {
 /**
  * Check that the total passed time in modifications is defined and below MAX_DELTA_TIME_SUM
  */
-async function spentTimeAndClicksAreValid(modifications, idleTime, serverTimeElapsed) {
+function spentTimeAndClicksAreValid(modifications, idleTime, serverTimeElapsed) {
 
     const totalDeltaTime = countTime(modifications) + idleTime
     const clickCount = modifications
@@ -91,54 +91,58 @@ async function readFromDatabase(auth) {
  * May or may not update gamestate to database depending on validity.
  * Always returns a valid new gamestate (or an empty one).
  */
-const verifyGamestate = functions.https.onCall(async (args, context) => {
+const verifyGamestate = functions
+    .runWith({memory: "256MB", timeoutSeconds: 10})
+    .region('europe-central2')
+    .https
+    .onCall(async (args, context) => {
 
-    // Return empty gamestate if user is not authenticated. 
-    if (!context?.auth?.uid) {
-        console.log("No authentication")
-        return new Gamestate()
-    }
+        // Return empty gamestate if user is not authenticated. 
+        if (!context?.auth?.uid) {
+            console.log("No authentication")
+            return new Gamestate()
+        }
 
-    const { modifications = [], idleTimeAfterModifications = 0 } = args
-    /** 
-     * Read gamestate and timestamp from database.
-     * ?? {} is there because readFromDatabase might return undefined.
-    */
-    let {gamestate: plainGamestate, timestamp} = await readFromDatabase(context.auth) ?? {}
-    //console.log("Old gamestate from database:", plainGamestate, timestamp)
+        const { modifications = [], idleTimeAfterModifications = 0 } = args
+        /** 
+         * Read gamestate and timestamp from database.
+         * ?? {} is there because readFromDatabase might return undefined.
+        */
+        let {gamestate: plainGamestate, timestamp} = await readFromDatabase(context.auth) ?? {}
+        //console.log("Old gamestate from database:", plainGamestate, timestamp)
 
-    // Convert plain JS object into Gamestate class instance
-    const oldGamestate = new Gamestate(plainGamestate)
+        // Convert plain JS object into Gamestate class instance
+        const oldGamestate = new Gamestate(plainGamestate)
 
-    const currentTime = Date.now()
-    /**
-     * Timestamp can be null if it's the user's first update.
-     * Therefore we assume the time to be MAX_DELTA_TIME_SUM milliseconds before the current time
-    */
-    timestamp = timestamp ?? currentTime - MAX_DELTA_TIME_SUM
+        const currentTime = Date.now()
+        /**
+         * Timestamp can be null if it's the user's first update.
+         * Therefore we assume the time to be MAX_DELTA_TIME_SUM milliseconds before the current time
+        */
+        timestamp = timestamp ?? currentTime - MAX_DELTA_TIME_SUM
 
 
-    /**
-     * Return old gamestate from database if the user hasn't sent an update in MAX_DELTA_TIME_SUM
-     * or if the time spent by the client is longer than the server time since the previous update.
-     * This is necessary to prevent cheating.
-    */
-    if (!(await spentTimeAndClicksAreValid(modifications, idleTimeAfterModifications, currentTime - timestamp))) {
-        console.log("spentTimeAndClicksAreValid is not valid!")
-        return oldGamestate
-    }
+        /**
+         * Return old gamestate from database if the user hasn't sent an update in MAX_DELTA_TIME_SUM
+         * or if the time spent by the client is longer than the server time since the previous update.
+         * This is necessary to prevent cheating.
+        */
+        if (!(spentTimeAndClicksAreValid(modifications, idleTimeAfterModifications, currentTime - timestamp))) {
+            console.log("spentTimeAndClicksAreValid is not valid!")
+            return oldGamestate
+        }
 
-    /**
-     * Build new gamestate by applying modifications to the old one.
-     * This will be equal to the old gamestate if the modifications couldn't be applied (attempted cheating or a bug).
-     */
-    const newGamestate = buildGamestate(oldGamestate, modifications, idleTimeAfterModifications)
-    
-    // Gamestate has to be converted to a plain JS object to write it to Firestore.
-    writeToDatabase(context.auth, {...newGamestate}, currentTime)
+        /**
+         * Build new gamestate by applying modifications to the old one.
+         * This will be equal to the old gamestate if the modifications couldn't be applied (attempted cheating or a bug).
+         */
+        const newGamestate = buildGamestate(oldGamestate, modifications, idleTimeAfterModifications)
+        
+        // Gamestate has to be converted to a plain JS object to write it to Firestore.
+        writeToDatabase(context.auth, {...newGamestate}, currentTime)
 
-    // Return updated and verified gamestate to client
-    return newGamestate
+        // Return updated and verified gamestate to client
+        return newGamestate
 });
 
 module.exports = { verifyGamestate }
