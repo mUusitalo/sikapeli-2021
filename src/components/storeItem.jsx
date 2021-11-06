718b687a291a9d3daaaa1278d7357ee516c9be16
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-11-07 00:04:08 +0200

    add rates to buttons

--- src/components/storeItem.jsx
+++ src/components/storeItem.jsx
@@ -2,6 +2,7 @@ import { calculatePrice, } from  "../game-logic/purchase-logic.js"
 import { GamestateVariables, } from "../game-logic/gamestate-variables";
 import { Icons, } from './icons.jsx'
 import { Gamestate, } from '../game-logic/gamestate'
+import { generatorSpecs, } from '../game-logic/generators'
 
 const StoreItem = ({gamestate, generator, onClick}) =>(
 	<button onClick={() => onClick(generator)} disabled={!gamestate.canBuy(generator)} class='store-item'>
@@ -12,6 +13,10 @@ const StoreItem = ({gamestate, generator, onClick}) =>(
 				<div class="item-price">
 					{gamestate.formatNumber(calculatePrice(generator, gamestate[generator]+1))}
 					<img class="price-icon" src={Icons[GamestateVariables.PEKONI]}/>
+				</div>
+				<div class="item-price">
+					{(generator != GamestateVariables.RESET) ? generatorSpecs[generator].rate : 0}
+					<img class="price-icon" src={Icons[GamestateVariables.PEKONI]}/> / S
 				</div>
 			</div>
 		</div>
