e07ba583aec49cde2f8d4a2c071441aad52636ee
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-11-07 20:14:26 +0200

    Clean up store items

--- src/components/storeItem.jsx
+++ src/components/storeItem.jsx
@@ -1,27 +1,52 @@
 import { calculatePrice, } from  "../game-logic/purchase-logic.js"
 import { GamestateVariables, } from "../game-logic/gamestate-variables";
 import { Icons, } from './icons.jsx'
-import { Gamestate, } from '../game-logic/gamestate'
+//import { Gamestate, } from '../game-logic/gamestate'
 import { generatorSpecs, } from '../game-logic/generators'
 
-const StoreItem = ({gamestate, generator, onClick}) =>(
-	<button onClick={() => onClick(generator)} disabled={!gamestate.canBuy(generator)} class='store-item'>
+const StoreItem = ({gamestate, generator: gamestateVariable, onClick}) => {
+
+
+	return (<button onClick={() => onClick(gamestateVariable)} disabled={!gamestate.canBuy(gamestateVariable)} class='store-item'>
 		<div class="item-info-with-icon">
-			<img class="icon" src={Icons[generator]} alt={GamestateVariables[generator]}/>
+			<img class="icon" src={Icons[gamestateVariable]} alt={GamestateVariables[gamestateVariable]}/>
 			<div class="item-info">
-				<div class="item-name">{generator}</div>
+				<div class="item-name">{gamestateVariable}</div>
 				<div class="item-price">
-					{gamestate.formatNumber(calculatePrice(generator, gamestate[generator]+1), 2)}
-					<img class="price-icon" src={Icons[GamestateVariables.PEKONI]}/>
+					{gamestate.formatNumber(calculatePrice(gamestateVariable, gamestate[gamestateVariable]+1), 2)}
+					<BaconIcon/>
 				</div>
 				<div class="item-price">
-					{(generator != GamestateVariables.RESET) ? generatorSpecs[generator].rate : 0}
-					<img class="price-icon" src={Icons[GamestateVariables.PEKONI]}/> / S
+					<GenerationRate{...{gamestate, gamestateVariable}}/>
 				</div>
 			</div>
 		</div>
-	<div class="item-amount">{gamestate[generator]}</div>
-	</button>
-)
+	<div class="item-amount">{gamestate[gamestateVariable]}</div>
+	</button>)
+}
+
+const GenerationRate = ({gamestate, gamestateVariable}) => {
+	switch(gamestateVariable) {
+		case GamestateVariables.RESET:
+			return null
+
+		case GamestateVariables.KERROIN:
+			return (
+				<>
+					+1%(<BaconIcon/>/s)/click
+				</>
+			)
+
+		default:
+			const generationRate = generatorSpecs[gamestateVariable].rate
+			return (
+				<>
+					{generationRate}<BaconIcon/>/s
+				</>
+			)
+	}
+}
+
+const BaconIcon = () => <img class="price-icon" alt="bacon" src={Icons[GamestateVariables.PEKONI]}/>
 
 export {StoreItem}
