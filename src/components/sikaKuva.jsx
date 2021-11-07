47b3d40548bcf661970f01b347f8f666893058a0
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-11-07 17:35:37 +0200

    add ghetto +1

--- src/components/sikaKuva.jsx
+++ src/components/sikaKuva.jsx
@@ -4,11 +4,16 @@ import SIKA_BACKGROUND from '../assets/glow.svg'
 
 const SikaKuva = ({handleClick}) => {
 	const [animated, setAnimated] = useState(0)
+	const [plus, setPlus] = useState(0)
 	return(
 		<div id='sika-img-div'>
+		<div id='plus-one-div'>
+		{Array(plus).fill(0).map((_, i) => <div class='plus-one' onAnimationEnd={() => setPlus(Math.floor(plus/2))}>+1</div>)}
+		</div>
 		<img
 		 onClick={() =>{setAnimated(1);
-						   handleClick();}}
+		 				setPlus(plus+1);
+						handleClick();}}
 		 onAnimationEnd={() => setAnimated(0)}
 		 src={SikaImg}
 		 id='sika-img'
